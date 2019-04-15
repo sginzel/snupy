4252de655781ec62f39a59ecd6c4686e460a8125
sginzel <sebastian.ginzel@inf.h-brs.de>
2019-04-15 10:53:22 +0200

    fixed indentation and default behaviour.

--- app/models/generic_list.rb
+++ app/models/generic_list.rb
@@ -17,70 +17,70 @@ class GenericList < ActiveRecord::Base
   
   def read_data(opts)
   	# raise NotImplementedError.new("read_data is not implemented for this class")
-  	opts = opts.reverse_merge({data: nil, file: nil, idx: 0, sep: "\t", has_header: false})
+  	opts = opts.reverse_merge({data: nil, file: nil, idx: 0, sep: "\t", has_header: false, header: false})
   	data = opts[:data]
-		file = opts[:file]
-		idxs = opts[:idx].split(",").map(&:to_i)
-		sep = opts[:sep]
-		has_header = opts[:header]
-		if !file.nil? then
-			# content = file.read
-			file = file
-		elsif data.to_s != "" then
-			# fin = File.new(oldsmpl["file"], "r")
-			file = File.new("/tmp/#{data.hash}", "w+")
-			file.write(data.to_s)
-			file.seek(0)
-			# content = data.to_s
-		else
-			self.errors[:base] << "Missing data."
-			return false
+	file = opts[:file]
+	idxs = opts[:idx].split(",").map(&:to_i)
+	sep = opts[:sep]
+	has_header = opts[:header]
+	if !file.nil? then
+		# content = file.read
+		file = file
+	elsif data.to_s != "" then
+		# fin = File.new(oldsmpl["file"], "r")
+		file = File.new("/tmp/#{data.hash}", "w+")
+		file.write(data.to_s)
+		file.seek(0)
+		# content = data.to_s
+	else
+		self.errors[:base] << "Missing data."
+		return false
+	end
+	# content.gsub!(/\r\n?/, "\n")
+	items = []
+	items2add = []
+	lineno = 0
+	header = []
+    begin
+		CSV.read(file,
+						 col_sep: sep,
+						 quote_char: '"',
+						 headers: has_header,
+						 return_headers: true,
+						 skip_blanks: true,
+						 force_quotes: false,
+						 encoding: "UTF-8")
+		.each do |cols|
+		    if lineno == 0 then
+		        if has_header
+		            header = cols.headers
+		            lineno = lineno + 1
+			        next
+		        else
+		            header = (0...(cols.size)).to_a.map(&:to_s)
+		        end
+		        lineno = lineno + 1
+		    end
+		    record = Hash[header.each_with_index.map{|c,i| [header[i], cols[i].to_s.gsub("\n", " ").force_encoding("utf-8")]}]
+			items2add << create_item(record, header, cols, idxs)
+			lineno = lineno + 1
 		end
-		# content.gsub!(/\r\n?/, "\n")
-		items = []
-		items2add = []
-		lineno = 0
-		header = []
-		begin
-			CSV.read(file, 
-							 col_sep: sep, 
-							 quote_char: '"', 
-							 headers: has_header, 
-							 return_headers: true,
-							 skip_blanks: true,
-							 force_quotes: false,
-							 encoding: "UTF-8")
-			.each do |cols|
-			 	if lineno == 0 then
-			 		if has_header
-			 			header = cols.headers
-			 			lineno = lineno + 1
-				 		next
-			 		else
-			 			header = (0...(cols.size)).to_a.map(&:to_s)
-			 		end
-			 		lineno = lineno + 1
-			 	end
-			 	record = Hash[header.each_with_index.map{|c,i| [header[i], cols[i].to_s.gsub("\n", " ").force_encoding("utf-8")]}]
-				items2add << create_item(record, header, cols, idxs)
-				lineno = lineno + 1
-			end
-		rescue => e
-			self.errors[:base] << "[LINE ##{lineno+1}] " + e.message
-			return false
+	rescue => e
+		self.errors[:base] << "[LINE ##{lineno+1}(#{file.path})] " + e.message
+		return false
+	end
+	
+	if items2add.size > 0 then
+		self.transaction do
+			self.items = [] if self.items.size > 0
+			self.items = items2add
 		end
-			
-		if items2add.size > 0 then
-			self.transaction do 
-				self.items = [] if self.items.size > 0
-				self.items = items2add
-			end
-		end
-		if data.to_s != "" then
-			file.close
-			File.unlink(file.path)
-		end
-		true
+	end
+	if data.to_s != "" then
+		file.close
+		File.unlink(file.path)
+	end
+	true
   end
   
   def create_item(record, header, cols, idxs)
